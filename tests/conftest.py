be6e7e269c9c5b025c4f07c0f5af1b8861d581ef
ongleam-eskim <eunsik.kim@ongleam.com>
2026-01-30 10:59:59 +0900

    refactor: Introduce application bootstrap, abstract email notifications to a generic notifications adapter, and update handler tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,22 +18,29 @@ pytest.register_assert_rewrite("tests.e2e.api_client")
 
 
 @pytest.fixture
-def in_memory_db():
+def in_memory_sqlite_db():
     engine = create_engine("sqlite:///:memory:")
     metadata.create_all(engine)
     return engine
 
 
 @pytest.fixture
-def sqlite_session_factory(in_memory_db):
+def sqlite_session_factory(in_memory_sqlite_db):
     start_mappers()
-    yield sessionmaker(bind=in_memory_db)
+    yield sessionmaker(bind=in_memory_sqlite_db)
     clear_mappers()
 
 
 @pytest.fixture
 def sqlite_session(sqlite_session_factory):
     return sqlite_session_factory()
+
+
+@pytest.fixture
+def mappers():
+    start_mappers()
+    yield
+    clear_mappers()
 
 
 @retry(stop=stop_after_delay(10))
@@ -54,7 +61,7 @@ def wait_for_redis_to_come_up():
 
 @pytest.fixture(scope="session")
 def postgres_db():
-    engine = create_engine(config.get_postgres_uri())
+    engine = create_engine(config.get_postgres_uri(), isolation_level="SERIALIZABLE")
     wait_for_postgres_to_come_up(engine)
     metadata.create_all(engine)
     return engine
